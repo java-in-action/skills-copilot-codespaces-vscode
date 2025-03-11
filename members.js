e4c286f92dc4c995266001c6828ff46c3bc002e1
java-in-action <47753654+java-in-action@users.noreply.github.com>
2025-03-10 19:42:20 -0600

    Create members.js

---
+++ members.js
@@ -0,0 +1,19 @@
+function skillsMembers() {
+  return {
+    name: 'skills-copilot-codespaces-vscode',
+    members: [
+      {
+        name: 'github.com/alexc-49',
+        role: 'maintainer',
+      },
+      {
+        name: 'github.com/alexc-49',
+        role: 'contributor',
+      },
+      {
+        name: 'github.com/alexc-49',
+        role: 'contributor',
+      },
+    ]
+  };
+}
